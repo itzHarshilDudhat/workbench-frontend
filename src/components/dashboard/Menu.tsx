feeeaf5624eef2bac502ddbaadcd6274a6d8fe7e
Harshil Dudhat <110517526+itzHarshilDudhat@users.noreply.github.com>
2024-01-18 22:40:27 +0530

    close sidebar when click on logout

--- src/components/dashboard/Menu.tsx
+++ src/components/dashboard/Menu.tsx
@@ -123,7 +123,10 @@ const Menu: FC = () => {
             </li>
             <li>
               <div
-                onClick={() => setLogoutPopup(true)}
+                onClick={() => {
+                  setSidebarVisible(false);
+                  setLogoutPopup(true);
+                }}
                 className="cursor-pointer flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
